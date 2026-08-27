caffa1b33f2b2142db1c4cc5c7ea25c4b2608adc
Newman <rohithnewman123@gmail.com>
2026-08-27 14:57:49 +0530

    tabler package add

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import {svelte} from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), svelte(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
